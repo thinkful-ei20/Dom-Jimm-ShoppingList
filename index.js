4ae9a4f5e90946de80bd4938276dea098ad00f49
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-06 11:08:55 -0400

    Added commented user stories

---
+++ index.js
@@ -0,0 +1,7 @@
+// USER STORY 1: Shopping list should be rendered to page
+
+// USER STORY 2: User should be able to add item to shopping list
+
+// USER STORY 3: User should be able to check items on the list
+
+// USER STORY 4: User should be able to delete items from the list
